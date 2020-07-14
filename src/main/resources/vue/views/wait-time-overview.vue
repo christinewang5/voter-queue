80cebb0e9f0605d6e44b3b3a53efb8d35c7f8a2c
Christine Wang <christinewang@fb.com>
2020-07-14 12:24:12 -0500

    table looks better now

--- src/main/resources/vue/views/wait-time-overview.vue
+++ src/main/resources/vue/views/wait-time-overview.vue
@@ -1,20 +1,22 @@
 <template id="wait-time-overview">
     <div class="container">
         <img src="/Images/header3.png" width="910" height="193" alt="banner">
-        <table>
-            <thead>
-            <tr>
-                <th>Precinct</th>
-                <th>Wait Time</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="wt in waitTimes">
-                <td>{{wt.precinct}}</td>
-                <td>{{wt.waitTime}}</td>
-            </tr>
-            </tbody>
-        </table>
+          <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                    <tr>
+                        <th>Precinct</th>
+                        <th>Wait Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="wt in waitTimes">
+                        <td>{{wt.precinct}}</td>
+                        <td>{{wt.waitTime}}</td>
+                    </tr>
+                </tbody>
+            </template>
+      </v-simple-table>
     </div>
 </template>
 <script>
@@ -34,39 +36,6 @@
 <style>
     div.container {
         position: relative;
-    }
-    ul.wait-time-overview-list {
-        padding: 0;
-        list-style: none;
-    }
-    ul.wait-time-overview-list li {
-        display: block;
-        padding: 16px;
-        border-bottom: 1px solid #ddd;
-    }
-    table {
-        border: 2px solid #42b983;
-        border-radius: 3px;
-        background-color: #fff;
         text-align: center;
-    }
-    th {
-        background-color: #42b983;
-        color: rgba(255, 255, 255, 0.66);
-        cursor: pointer;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-    }
-
-    td {
-        background-color: #f9f9f9;
-    }
-
-    th,
-    td {
-        min-width: 120px;
-        padding: 10px 20px;
     }
 </style>
