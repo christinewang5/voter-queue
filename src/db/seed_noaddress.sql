51ebde2975e0545ae069024c85cb4ec12f8a703d
John Berberian <jeb.study@gmail.com>
2020-07-28 12:16:04 -0400

    Added no-address db seed

---
+++ src/db/seed_noaddress.sql
@@ -0,0 +1,13 @@
+-- precinct data seed names
+INSERT INTO precinct_names(precinct, name) VALUES
+(0, 'Rice Memorial Center'),
+(1, 'Girl Scouts of San Jacinto Council'),
+(2, 'Acres Homes Multi Service Center'),
+(3, 'Adam Elementary School'),
+(4, 'Albright Middle School'),
+(5, 'Aldine Middle School'),
+(6, 'Alexander Elementary School'),
+(7, 'Alice Johnson Junior High School'),
+(8, 'BakerRipley East Aldine Campus'),
+(9, 'Cypress Fairbanks Exhibit Center'),
+(10, 'Harris County Courthouse Annex Num. 8');
